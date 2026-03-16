e02a18de83291c9673b73f753a57821cbafabab0
Eloah Sofia <eloah.sofia@alunos.ifsuldeminas.edu.br>
2026-03-16 08:26:41 -0300

    Aula2 - Estrutura de condição e Seleção

---
+++ aula2/if.py
@@ -0,0 +1,11 @@
+# Condicional IF
+
+# Entrada
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+3 # Processamento
+if(idade < 18):
+    autorizacao = input("Os pais autorizazam a viagem [SIM/NÃO]")
+
+print(f"Realizando o Embarque de {nome}")
---
+++ aula2/if_else.py
@@ -0,0 +1,12 @@
+# IF - Estrutura Condicional (se fulunano faz isso)
+
+#Variável + conversão de srt em real (float) + Entrada
+media = float(input("Digite a média do aluno:"))
+
+#Processamento
+if media >=6:
+    #caminho do sim
+    print("Você está aprovado: ")
+else:
+    #caminho do não
+    print("Você foi reprovado: ")
