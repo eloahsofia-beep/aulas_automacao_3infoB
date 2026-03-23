ab863006ab6a1756c46a426ac9667b8e9e9b7e58
Eloah Sofia <eloah.sofia@alunos.ifsuldeminas.edu.br>
2026-03-23 08:45:00 -0300

    Aula 3

---
+++ aula3/for2.py
@@ -0,0 +1,15 @@
+#Estrutura de repetição for:
+#percorrer listas
+#Lista estrutura de dados composta (armazena diversos valores)
+#variável que possui vários valores
+
+nomes = ['Ivan', 'Jesuely', 'Mariana', 'Eloah']
+print(nomes)
+
+#Imprime todos os nomes da lista ultilizando range
+for i in range(4):
+    print(nomes[i])
+
+#Imprime todos os nomes usando interador de lista
+for nome in nomes:
+    print(nome)
